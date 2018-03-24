e9e78e1d2494fe82dc1fa865f305126832a59bf7
Raul <raulfernandezsamc@gmail.com>
2018-03-25 00:59:50 +0100

    Informacio activities

--- app/src/main/java/com/example/hackbdx/MainActivity.java
+++ app/src/main/java/com/example/hackbdx/MainActivity.java
@@ -4,25 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button btn;
+    EditText editText;
+    EditText editText2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButtonListener();
+        editText = (EditText) findViewById(R.id.textsitio);
+        editText2 = (EditText) findViewById(R.id.textnumero);
     }
 
     public void ButtonListener() {
         btn = (Button) findViewById(R.id.btnSubmit);
+
+
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                String dato = editText.getText().toString();
+                String dato2 = editText2.getText().toString();
                 Intent i = new Intent("com.example.hackbdx.TemperatureActivity");
+                i.putExtra("dato",dato);
+                i.putExtra("dato2", dato2);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/hackbdx/TemperatureActivity.java
+++ app/src/main/java/com/example/hackbdx/TemperatureActivity.java
@@ -1,13 +1,27 @@
 package com.example.hackbdx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TemperatureActivity extends AppCompatActivity {
+    TextView texto, texto2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
+        texto = (TextView) findViewById(R.id.text);
+        texto2 = (TextView) findViewById(R.id.text2);
+        Intent i = getIntent();
+        Bundle extras = i.getExtras();
+        if (extras != null) {
+            String dato = (String)extras.get("dato");
+            String dato2 = extras.getString("dato2");
+            texto.setText(dato);
+            texto2.setText(dato2);
+        }
+
     }
 }
